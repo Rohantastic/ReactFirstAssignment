b3abe6bb8c0fe944bb335e085733025c33ac127c
rohantastic <rohantastic@outlook.com>
2023-09-27 22:45:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <>
+    <h1>Lets Get Started</h1>
+    <ExpenseItem/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return (
+        <>
+        <h1>ExpenseItem</h1>
+        <h1>
+        Expense Items</h1>
+        <h1>Food</h1> <h2>Rs 10</h2>
+        <h1>Petrol</h1> <h2>Rs 100</h2>
+        <h1>Movies</h1> <h2>Rs 200</h2>
+        </>
+    )
+}
+
+export default ExpenseItem;
