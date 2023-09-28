d29d954218a9fc2be9cb327d06dc713c0d56097e
rohantastic <rohantastic@outlook.com>
2023-09-28 20:57:44 +0530

    update

--- Downloads/react-complete-guide-code-04-react-state-events/react-complete-guide-code-04-react-state-events/code/02-adding-form-inputs/src/components/NewExpense/ExpenseForm.js
+++ Downloads/react-complete-guide-code-04-react-state-events/react-complete-guide-code-04-react-state-events/code/02-adding-form-inputs/src/components/NewExpense/ExpenseForm.js
@@ -1,32 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
 
-  const [enteredTitle,setEnteredTitle] = useState('');
-  const [enteredAmount,setEnteredAmount] = useState('');
-  const [enteredDate,setEnteredDate] = useState('');
-
-  const titleChangeHandler = (event)=>{
+  const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
 
-  const amountChangeHandler = (event)=>{
+  const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-  }
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+  };
 
-  const dateChangeHandler = (event)=>{
+  const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-  }
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
 
-  const submitHandler = (event)=>{
+  const submitHandler = (event) => {
     event.preventDefault();
-    const expenseData={
-      title:enteredTitle,
+
+    const expenseData = {
+      title: enteredTitle,
       amount: enteredAmount,
-      date: new Date(enteredDate)
+      date: new Date(enteredDate),
     };
 
     props.onSaveExpenseData(expenseData);
@@ -40,17 +59,33 @@ const ExpenseForm = (props) => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text'value={enteredTitle} onChange={titleChangeHandler}/>
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' value={enteredAmount}onChange={amountChangeHandler} min='0.01' step='0.01' />
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' value={enteredDate}onChange={dateChangeHandler} min='2019-01-01' max='2022-12-31' />
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
-      </div> 
+      </div>
       <div className='new-expense__actions'>
         <button type='submit'>Add Expense</button>
       </div>
